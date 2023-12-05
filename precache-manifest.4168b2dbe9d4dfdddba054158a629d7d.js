79ec408fa00cc495b288ad664a98c21c7f0765b7
spushpender521 <151756705+spushpender521@users.noreply.github.com>
2023-12-04 22:30:43 -0800

    Updates

---
+++ precache-manifest.4168b2dbe9d4dfdddba054158a629d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda9a2d04e774dc4f8bf887e8d885342",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "5cbab376814a2d12e9c8",
+    "url": "/React/static/css/main.2b0446f9.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/React/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/React/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "5cbab376814a2d12e9c8",
+    "url": "/React/static/js/main.f45aeb01.chunk.js"
+  },
+  {
+    "revision": "756531303b9b664d5415",
+    "url": "/React/static/js/runtime~main.4100cd90.js"
+  }
+]);
